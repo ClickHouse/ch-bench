f3a8d9de92bd37b9289a7fbe4ab5d57f888d64cc
Aleksandr Razumov <ernado@ya.ru>
2021-12-23 02:55:48 +0300

    feat: update java results
    0:10.56 702_812kb

--- ch-bench-java/run.sh
+++ ch-bench-java/run.sh
@@ -5,7 +5,7 @@ set -e
 : ${DB_HOST:="localhost"}
 : ${DB_PORT:="8123"}
 : ${DRIVER_VERSION:="0.3.2-test3"}
-: ${COMPRESS:="true"}
+: ${COMPRESS:="false"}
 
 JDBC_DRIVER="clickhouse-jdbc-$DRIVER_VERSION-http.jar"
 
@@ -24,4 +24,4 @@ else
 fi
 
 echo "Running..."
-time java -DdbHost="$DB_HOST" -DdbPort="$DB_PORT" -Dcompress="${COMPRESS}" -cp ".:$JDBC_DRIVER" Main $@
+/usr/bin/time -v java -DdbHost="$DB_HOST" -DdbPort="$DB_PORT" -Dcompress="${COMPRESS}" -cp ".:$JDBC_DRIVER" Main $@
