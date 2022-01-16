40727845efa1eca58f92e4522bb70d0b420e410e
sundyli <543950155@qq.com>
2022-01-16 18:19:06 +0800

    Use compression none

--- ch-bench-rust-driver/src/main.rs
+++ ch-bench-rust-driver/src/main.rs
@@ -1,10 +1,11 @@
 use clickhouse_driver::prelude::*;
-use std::error::Error;
+use std::{error::Error, time::Instant};
 
 async fn execute(database_url: String) -> Result<(), Box<dyn Error>> {
     let pool = Pool::create(database_url.as_str())?;
     let mut conn = pool.connection().await?;
     let mut total: u64 = 0;
+    let start = Instant::now();
 
     let mut result = conn
         .query("SELECT number FROM system.numbers_mt LIMIT 500000000")
@@ -13,14 +14,14 @@ async fn execute(database_url: String) -> Result<(), Box<dyn Error>> {
     while let Some(block) = result.next().await? {
         total += block.row_count();
     }
-    println!("Rows: {}", total);
-
+    let elapsed = start.elapsed();
+    println!("Rows: {}, elspsed: {} ms", total, elapsed.as_millis());
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    execute("tcp://localhost:9000".to_string()).await?;
+    execute("tcp://localhost:9000?compression=none".to_string()).await?;
 
     Ok(())
 }
