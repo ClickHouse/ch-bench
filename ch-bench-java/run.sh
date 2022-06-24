88cd233dc8e8f9dd128ea63751a52326c875e045
Zhichun Wu <zhicwu@gmail.com>
2022-06-24 22:49:31 +0800

    Update Java benchmark

--- ch-bench-java/run.sh
+++ ch-bench-java/run.sh
@@ -2,26 +2,28 @@
 
 set -e
 
-: ${DB_HOST:="localhost"}
-: ${DB_PORT:="8123"}
-: ${DRIVER_VERSION:="0.3.2-test3"}
-: ${COMPRESS:="false"}
+: ${SQL:="SELECT number FROM system.numbers_mt LIMIT 500000000"}
+: ${URL:='http://localhost?!compress&format=RowBinaryWithNamesAndTypes'}
+: ${VER:="0.3.2-patch10"}
 
-JDBC_DRIVER="clickhouse-jdbc-$DRIVER_VERSION-http.jar"
+: ${OPTS:=""}
+#OPTS="-XX:+UnlockDiagnosticVMOptions -XX:+DebugNonSafepoints -XX:+FlightRecorder -XX:StartFlightRecording=disk=false,filename=recorded.jfr,settings=profile"
+PKG="clickhouse-jdbc-$VER-all.jar"
 
-if [ ! -f "$JDBC_DRIVER" ]; then
-    echo "Downloading $JDBC_DRIVER..."
-    curl -sOL "https://github.com/ClickHouse/clickhouse-jdbc/releases/download/v$DRIVER_VERSION/$JDBC_DRIVER"
+
+if [ ! -f "$PKG" ]; then
+    echo "Downloading $PKG..."
+    curl -sOL "https://github.com/ClickHouse/clickhouse-jdbc/releases/download/v$VER/$PKG"
 else
-    echo "Found $JDBC_DRIVER"
+    echo "Found $PKG"
 fi
 
 if [ ! -f "Main.class" ]; then
     echo "Compiling..."
-    javac -cp "$JDBC_DRIVER" Main.java
+    javac -cp "$PKG" Main.java
 else
     echo "Found compiled class"
 fi
 
 echo "Running..."
-/usr/bin/time -v java -DdbHost="$DB_HOST" -DdbPort="$DB_PORT" -Dcompress="${COMPRESS}" -cp ".:$JDBC_DRIVER" Main $@
+\time -v java $OPTS -Durl="$URL" -Dsql="$SQL" -cp ".:$PKG" Main $@
